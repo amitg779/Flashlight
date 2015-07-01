c6e9f887919e19f975c01c23d99f3ccd1e0c7158
amitg779 <amitg779@gmail.com>
2015-07-01 19:27:00 +0300

    asd

--- javascript/custom.js
+++ javascript/custom.js
@@ -1,17 +1,40 @@
-function flash() {
-window.plugins.flashlight.available(function(isAvailable) {
-  if (isAvailable) {
-
-    // switch on
-    window.plugins.flashlight.switchOn(); // success/error callbacks may be passed
-
-    // switch off after 3 seconds
-    setTimeout(function() {
-      window.plugins.flashlight.switchOff(); // success/error callbacks may be passed
-    }, 3000);
-
-  } else {
-    alert("Flashlight not available on this device");
-  }
+$("#flipFlashlight").change(function () {
+    if ($("#flipFlashlight").is(":checked")) {
+        window.plugins.flashlight.available(function (isAvailable) {
+            if (isAvailable) {
+                // switch on
+                window.plugins.flashlight.switchOn(); // success/error callbacks may be passed
+            } else {
+                alert("Flashlight not available on this device");
+            }
+        });
+    }
+    else {
+        window.plugins.flashlight.available(function (isAvailable) {
+            if (isAvailable) {
+                // switch off
+                window.plugins.flashlight.switchOff(); // success/error callbacks may be passed
+            } else {
+                alert("Flashlight not available on this device");
+            }
+        });
+    }
 });
+
+function flash() {
+    window.plugins.flashlight.available(function (isAvailable) {
+        if (isAvailable) {
+
+            // switch on
+            window.plugins.flashlight.switchOn(); // success/error callbacks may be passed
+
+            // switch off after 3 seconds
+            setTimeout(function () {
+                window.plugins.flashlight.switchOff(); // success/error callbacks may be passed
+            }, 3000);
+
+        } else {
+            alert("Flashlight not available on this device");
+        }
+    });
 }
