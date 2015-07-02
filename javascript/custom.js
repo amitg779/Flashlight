207a2b737f30fa07d30d9a6732994c0a6ac3c562
amitg779 <amitg779@gmail.com>
2015-07-02 18:00:25 +0300

    almost there

--- javascript/custom.js
+++ javascript/custom.js
@@ -1,60 +1,26 @@
 $(document).ready(function () {
     var state = "off";
 
-    // Listener for Flashlight Toggle
-    $("#flipFlashlight").change(function () {
-        if ($(this).is(":checked")) {
-            flash("on");
-        }
-        else {
-            flash("off");
-        }
-    });
-
-
     // Listener for Toggle Button
     $("#btnToggle").on("vmousedown", function () {
         if (state == "off") {
-            flash("on");
             state = "on";
+            $(this).addClass("button-on");
+            flash("on");
         }
         else {
-            flash("off");
             state = "off";
+            $(this).removeClass("button-on");
+            flash("off");
         }
     });
 
-
-    // Listener for Pulse Button
+    // Listeners for Pulse Button
     $("#btnPulse").on("vmousedown", function () {
         flash("on");
     });
-
     $("#btnPulse").on("vmouseup", function () {
         flash("off");
-    });
-
-
-    // Listener for Flicker Toggle
-    var intrFlicker;
-    $("#flipFlicker").change(function () {
-        if ($(this).is(":checked")) {
-            intrFlicker = setInterval(function () {
-                switch (state) {
-                    case "off":
-                        state = "on";
-                        flash("on");
-                        break;
-                    case "on":
-                        state = "off";
-                        flash("off");
-                        break;
-                }
-            }, 1000);
-        }
-        else {
-            clearInterval(intrFlicker);
-        }
     });
 });
 
